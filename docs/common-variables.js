f3fbc6c12a93f92136689b1c3228f5895e3f1341
Travis Coleman <travis.coleman@genesys.com>
2022-09-20 15:20:46 -0400

    updated variables

--- docs/common-variables.js
+++ docs/common-variables.js
@@ -1,13 +1,13 @@
 module.exports = {
     // Genesys Cloud Client Creedentials OAuth
-    clientId: "your-client-id",
-    clientSecret: "your-client-secret",
+    clientId: "bd9fdc15-afed-4e8e-b3b3-69dbf073b9f8",
+    clientSecret: "PBBzTl4kVJsmINjiv7vM6IU2KXrlQPB1pFNZPG7fcac",
     // Open Messaging
-    open_messaging_secret_token: "my-secret-token",
-    open_messaging_direct_messaging_id: "open-messaging-id-for-direct-message",
-    open_messaging_app_mention_id: "open-messaging-id-for-app-mention",
+    open_messaging_secret_token: "OpenMessaging",
+    open_messaging_direct_messaging_id: "051fb010-7ef9-437b-b343-4f6c0ab41315",
+    open_messaging_app_mention_id: "1da7e909-817a-4e4b-a939-e10fbbe86ec4",
     // Slack Configuration
-    app_verification_token: "app-verification-token", 
-    slack_bearer_token: "slack-bearer-token",
-    slack_app_mention_channel: "slack-channel-id"
+    app_verification_token: "nSzyidgHibK4AV0JkSiq1yo0", 
+    slack_bearer_token: "xoxb-4099640705141-4126453099168-3ajGBxwvdHVJ3ozqKAqny6QI",
+    slack_app_mention_channel: "C0430ET1MEX"
 };
